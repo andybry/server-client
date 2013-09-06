0256c0dd920346d849440d761e93243c285959c8
Andy Bryant <ar.bryant@btinternet.com>
2013-09-06 09:22:19 +0100

    Route /
    Redirect the root to the article page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ServerClient::Application.routes.draw do
 
+  get "/", to: redirect("/article")
   get "/template/:template", to: "template#index", template: /.*/, as: :templates
   get "/article", to: "article#index", as: :article
 
